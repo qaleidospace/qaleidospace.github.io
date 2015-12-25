b4d92b1f17c90a673bde5390e26fd4c4ed1b8296
Yuta Koshizawa <koher@koherent.org>
2015-12-26 01:11:34 +0900

    Implement searching tags

--- js/search.js
+++ js/search.js
@@ -1,19 +1,55 @@
-$(".ranking>.item>.main>.tags>.tag").click(function() {
-  var value = $(this).val()
-  // alert($(this).val());
+function searchTags(searchable, tags) {
+  $(".ranking>.item", searchable).each(function(index, item) {
+    var matched = tags.reduce(function(acc, tag) {
+      var contains = false;
+      $(".main>.tags>.tag", item).each(function(index, tagElement) {
+        contains |= tag == $(tagElement).val().toLowerCase();
+      });
+      return acc && contains;
+    }, true);
+    if (matched) {
+      $(item).show();
+    } else {
+      $(item).hide();
+    }
+  });
+}
 
-  var items = $(".ranking>.item>.main>.tags>.tag").filter(function(index) {
-    return $(this).val() == value;
+function addTag(searchable, tag) {
+  if (/\s/.test(tag)) {
+    tag = "\"" + tag + "\"";
+  }
+
+  var input = $(".search input", searchable);
+  var inputValue = input.val();
+  input.val(inputValue == "" ? tag : inputValue + " " + tag);
+}
+
+function complementNull(value, alternative) {
+  return value != null ? value : alternative;
+}
+
+function getTags(searchable) {
+  var tagsString = $(".search input", searchable).val();
+  var multiTagExp = /"[^"]+"/g;
+  var tagExp = /\S+/g;
+  var tags = complementNull(tagsString.match(multiTagExp), []).map(function(tag) { return tag.substr(1, tag.length - 2); });
+  tagsString = tagsString.replace(multiTagExp, " ");
+  complementNull(tagsString.match(tagExp), []).forEach(function (tag) {
+    tags.push(tag);
   });
 
-  $(".ranking>.item").filter(function(index) {
-    var thisItem = $(this);
+  return tags.map(function(tag) { return tag.toLowerCase(); });
+}
 
-    var result = false;
-    items.each(function(item) {
-      result &= $.contains(thisItem, items);
-    });
+$(".searchable").each(function(index, searchable) {
+  $(".search button", searchable).click(function() {
+    searchTags(searchable, getTags(searchable));
+  });
 
-    return result;
-  }).hide();
+  $(".ranking>.item>.main>.tags>.tag", searchable).click(function() {
+    addTag(searchable, $(this).val());
+    searchTags(searchable, getTags(searchable));
+  });
 });
+
